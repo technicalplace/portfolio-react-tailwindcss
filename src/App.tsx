2af25aee22581504176edf66b29b570da147596e
Shunsuke Fujinawa <shun26497204@gmail.com>
2023-05-29 01:03:04 +0900

    :recycle: Appコンポーネントのheightの指定変更

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Link } from './components/Link/Link';
 
 function App() {
   return (
-      <div className='bg-gradient-to-r from-sky-700 from-30% via-sky-800 via-40% to-sky-950 to-70% w-ful h-full'>
-        <h1 className='text-8xl mx-auto w-80 pt-32 text-center text-emerald-500 title__font'>S.Tech</h1>
+      <div className='bg-gradient-to-r from-sky-700 from-30% via-sky-800 via-40% to-sky-950 to-70% w-full h-screen'>
+        <h1 className='text-8xl mx-auto w-80 pt-32 text-center text-emerald-500 title__font'>S.Tech!!!</h1>
         <Link />
       </div>
   );
