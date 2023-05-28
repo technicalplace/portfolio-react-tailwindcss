472c73d91591c468329204797b71a329554f7e91
Shunsuke Fujinawa <shun26497204@gmail.com>
2023-05-28 23:15:13 +0900

    :green_heart: READMEの移動

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import './App.css';
 import { Link } from './components/Link/Link';
 
 function App() {
-
-
   return (
       <div className='bg-gradient-to-r from-sky-700 from-30% via-sky-800 via-40% to-sky-950 to-70% w-ful h-full'>
         <h1 className='text-8xl mx-auto w-80 pt-32 text-center text-emerald-500 title__font'>S.Tech</h1>
